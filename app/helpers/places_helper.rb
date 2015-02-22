32bc26d84873fdc64b72175eee524f7430380d42
Daniel Valencia <danvalencia@gmail.com>
2015-02-21 23:42:57 -0800

    Styling show view and added 'Open in Google Maps' link

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,11 @@
 module PlacesHelper
+
+  def map_url_for(place)
+    if mobile_device?
+      "comgooglemaps://?center=#{place.latitude},#{place.longitude}&zoom=15"
+    else
+      "https://www.google.com/maps/@#{place.latitude},#{place.longitude},15z"
+    end
+
+  end
 end
