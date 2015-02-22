e3f119771ffa7022df75b2573c101c9420a8e2e4
Daniel Valencia <danvalencia@gmail.com>
2015-02-21 23:52:00 -0800

    Displaying marker in google maps links

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -2,9 +2,9 @@ module PlacesHelper
 
   def map_url_for(place)
     if mobile_device?
-      "comgooglemaps://?center=#{place.latitude},#{place.longitude}&zoom=15"
+      "comgooglemaps://?center=#{place.latitude},#{place.longitude}&q=#{place.latitude},#{place.longitude}&zoom=15"
     else
-      "https://www.google.com/maps/@#{place.latitude},#{place.longitude},15z"
+      "https://www.google.com/maps?z=15&q=#{place.latitude}+#{place.longitude}&ll=#{place.latitude}+#{place.longitude}"
     end
 
   end
